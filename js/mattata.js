e9a37ba4ef9c56b98a8859543355132d5a56dcb7
Matt <matthew@matthewhesketh.com>
2016-12-24 12:13:10 +0000

    More improvements

--- js/mattata.js
+++
@@ -1,6 +0,0 @@
-function collapseNavbar() {
-    $(".navbar").offset().top > 50 ? $(".navbar-fixed-top").addClass("top-nav-collapse") : $(".navbar-fixed-top").removeClass("top-nav-collapse")
-}
-$(window).scroll(collapseNavbar), $(document).ready(collapseNavbar), $(".navbar-collapse ul li a").click(function() {
-    $(this).closest(".collapse").collapse("toggle")
-});
